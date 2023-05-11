6be0ab0bb765b26a983bba77bc98dad7ec9001f8
qubo <346386694@qq.com>
2023-05-11 16:59:11 +0800

    first commit

--- run_leaser_measurement.sh
+++ run_leaser_measurement.sh
@@ -1,4 +1,9 @@
 #！/bin/bash
+mkdir DATA
+mkdir SAVE
 xhost +
-docker run --rm --network=host -itd --name leaser --privileged -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 qubo188612/ubuntu20.04:leaser
+docker run --rm --network=host -itd --name leaser \
+--mount type=bind,source=./SAVE,destination=/workspace/leaser/SAVE \
+--mount type=bind,source=./DATA,destination=/workspace/leaser/DATA \
+--privileged -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 qubo188612/ubuntu20.04:leaser
 
